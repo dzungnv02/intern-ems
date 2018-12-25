2ef33f4692a06dff8c50566e2e5acc0d597c6243
Nguyen Vu Dung <dungnv@nal.vn>
2018-12-25 15:57:02 +0700

    test webhook

--- public/test_webhook.php
+++ public/test_webhook.php
@@ -1,3 +1,7 @@
 <?php
+
+use function GuzzleHttp\json_encode;
 if (isset($_POST['crm']))
-    var_dump($_POST['crm']);
+    echo json_encode(['input' => $_POST['crm']]);
+else 
+    echo json_encode(['result' => 'no input']);
