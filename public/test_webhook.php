fb6ca70b097e8d3a033e973aae4d9bdeecdf2f93
Nguyen Vu Dung <dungnv@nal.vn>
2018-12-26 17:38:12 +0700

    dump all hook data

--- public/test_webhook.php
+++ public/test_webhook.php
@@ -1,10 +1,12 @@
 <?php
-if (isset($_POST['crm'])) {
-    $f_handle = fopen('/apps/icanread/intern-ems/storage/webhook.log', 'a+');
-    $data = json_encode(['input' => $_POST['crm'], 'time' => date('Y-m-d H:i:s')]) . "\n";
+var_dump($_POST);
 
-    fwrite($f_handle, json_encode($data));
-    fclose($f_handle);
-    }
-else 
-    echo json_encode(['result' => 'no input']);
+// if (isset($_POST['crm'])) {
+//     $f_handle = fopen('/apps/icanread/intern-ems/storage/webhook.log', 'a+');
+//     $data = json_encode(['input' => $_POST['crm'], 'time' => date('Y-m-d H:i:s')]) . "\n";
+
+//     fwrite($f_handle, json_encode($data));
+//     fclose($f_handle);
+//     }
+// else 
+//     echo json_encode(['result' => 'no input']);
