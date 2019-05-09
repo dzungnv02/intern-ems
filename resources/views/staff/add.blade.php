31593f7b075137744b37b7ce2db13e7ae9271e9f
Nguyen Vu Dung <dungnv@nal.vn>
2019-05-09 10:23:21 +0700

    change staff form

--- resources/views/staff/add.blade.php
+++ resources/views/staff/add.blade.php
@@ -9,35 +9,42 @@
  @section('content')
  <form action="" method="post" id="myForm" enctype="multipart/form-data">
  	<div class="form-group row">
- 		<label for="inputEmail3" class="col-sm-2 col-form-label">Email *</label>
+ 		<label for="inputEmail" class="col-sm-2 col-form-label">Email <i class="text-danger">*</i></label>
  		<div class="col-sm-8">
- 			<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
- 			<span id="errorEmail" class="bell errorEmail">Email không đúng định dạng!</span>
+ 			<input type="email" class="form-control" id="inputEmail" placeholder="Email">
+ 			<span id="errorEmail" class="bell errorEmail hidden">Email không đúng định dạng!</span>
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Mật khẩu *</label>
+ 		<label for="inputPassword3" class="col-sm-2 col-form-label">Mật khẩu <i class="text-danger">*</i></label>
  		<div class="col-sm-8">
  			<input type="password" class="form-control" id="inputPassword" placeholder="Mật khẩu">
- 			<span id="errorPassword" class="bell errorPassword">Mật khẩu ít nhất 8 kí tự!</span>
+ 			<span id="errorPassword" class="bell errorPassword hidden">Mật khẩu ít nhất 8 kí tự!</span>
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Nhập lại mật khẩu *</label>
+ 		<label for="inputPassword3" class="col-sm-2 col-form-label">Nhập lại mật khẩu <i class="text-danger">*</i></label>
  		<div class="col-sm-8">
  			<input type="password" class="form-control" id="inputPassword_1" placeholder="Nhập lại mật khẩu">
- 			<span id="errorPassword_1" class="bell errorPassword_1">Mật khẩu phải giống nhau!</span>
+ 			<span id="errorPassword_1" class="bell errorPassword_1 hidden">Mật khẩu phải giống nhau!</span>
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Tên nhân viên *</label>
+ 		<label for="inputPassword3" class="col-sm-2 col-form-label">Tên nhân viên <i class="text-danger">*</i></label>
  		<div class="col-sm-8">
  			<input type="text" class="form-control" id="inputName" placeholder="Tên nhân viên ">
- 			<span id="errorName" class="bell errorName">Tên nhân viên không được bỏ trống!</span>
+ 			<span id="errorName" class="bell errorName hidden">Tên nhân viên không được bỏ trống!</span>
  		</div>
 	 </div>
 	 <div class="form-group row">
-		<label for="inputPassword3" class="col-sm-2 col-form-label">Chi nhánh *</label>
+		<label for="inputPhone" class="col-sm-2 col-form-label">Điện thoại <i class="text-danger">*</i></label>
+		<div class="col-sm-8">
+			<input type="text" class="form-control" id="inputPhone" placeholder="Điện thoại ">
+			<span id="errorPhone" class="bell errorPhone hidden">Số điện thoại không đúng định dạng!</span>
+		</div>
+	</div>
+	 <div class="form-group row">
+		<label for="inputPassword3" class="col-sm-2 col-form-label">Chi nhánh <i class="text-danger">*</i></label>
 		<div class="col-sm-8">
 			<select id="branch_id">
 				<option value="">[Chọn chi nhánh]</option>
@@ -45,35 +52,27 @@
 		</div>
 	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Giới tính *</label>
+ 		<label for="inputPassword3" class="col-sm-2 col-form-label">Giới tính</label>
  		<div class="col-sm-8">
  			<select name="gender" id="inputGender">
  				<option value="1">Nam</option>
  				<option value="0">Nữ</option>
- 				<option value="2">Khác</option>
+ 				<option value="2" selected>Khác</option>
  			</select>
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Ngày Sinh *</label>
+ 		<label for="inputPassword3" class="col-sm-2 col-form-label">Ngày Sinh</label>
  		<div class="col-sm-8">
  			<input type="date" class="form-control" id="inputBirthDate">
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Địa chỉ *</label>
+ 		<label for="inputAddress" class="col-sm-2 col-form-label">Địa chỉ</label>
  		<div class="col-sm-8">
  			<input type="text" class="form-control" id="inputAddress" placeholder="Địa chỉ">
  		</div>
  	</div>
- 	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Điện thoại  *</label>
- 		<div class="col-sm-8">
- 			<input type="text" class="form-control" id="inputPhone" placeholder="Điện thoại ">
- 			<span id="errorPhone" class="bell errorPhone">Số điện thoại không đúng định dạng!</span>
- 		</div>
- 	</div>
- 	
  	<div class="form-group row">
  		<div class="col-sm-2">
  			<button class="btn btn-primary" id="addStaff" type="button">Thêm nhân viên</button>
--- resources/views/staff/edit.blade.php
+++ resources/views/staff/edit.blade.php
@@ -1,6 +1,5 @@
  @extends('layouts.master')
  @section('header')
- {{-- <link rel="stylesheet" href="{{asset('css/staff.css')}}"> --}}
  @endsection
  @section('title')
  Sửa nhân viên
@@ -8,24 +7,10 @@
  @section('content')
  <form action="" method="post" id="abs">
  	<div class="form-group row">
- 		<label for="inputEmail3" class="col-sm-2 col-form-label">Email *</label>
+ 		<label for="inputEmail" class="col-sm-2 col-form-label">Email *</label>
  		<div class="col-sm-8">
- 			<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+ 			<input type="email" class="form-control" id="inputEmail" placeholder="Email">
  			<span id="errorEmail" class="bell errorEmail">Email không đúng định dạng!</span>
- 		</div>
- 	</div>
- 	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Mật khẩu *</label>
- 		<div class="col-sm-8">
- 			<input type="password" class="form-control" id="inputPassword" placeholder="Mật khẩu">
- 			<span id="errorPassword" class="bell errorPassword">Mật khẩu ít nhất 8 kí tự!</span>
- 		</div>
- 	</div>
- 	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Nhập lại mật khẩu *</label>
- 		<div class="col-sm-8">
- 			<input type="password" class="form-control" id="inputPassword_1" placeholder="Nhập lại mật khẩu">
- 			<span id="errorPassword_1" class="bell errorPassword_1">Mật khẩu phải giống nhau!</span>
  		</div>
  	</div>
  	<div class="form-group row">
@@ -36,7 +21,7 @@
  		</div>
  	</div>
  	<div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Giới tính *</label>
+ 		<label for="inputGender" class="col-sm-2 col-form-label">Giới tính *</label>
  		<div class="col-sm-8">
  			<select name="gender" id="inputGender">
  				<option value="1">Nam</option>
@@ -64,14 +49,6 @@
  			<span id="errorPhone" class="bell errorPhone">Số điện thoại không đúng định dạng!</span>
  		</div>
  	</div>
- 	{{--  <div class="form-group row">
- 		<label for="inputPassword3" class="col-sm-2 col-form-label">Hình ảnh *</label>
- 		<div class="col-sm-8">
- 			<input type="file" class="form-control" id="inputFile">
- 			<span id="errorImg" class="bell errorImg">Ảnh không đúng định dạng!</span>
- 			<img src="" alt="" id="showImg" style="">
- 		</div>
- 	</div>  --}}
  	<div class="form-group row">
  		<div class="col-sm-2">
  			<button type="button" class="btn btn-primary" id="editStaff_1">Sửa nhân viên</button>
@@ -83,6 +60,5 @@
  </form>
  @endsection
  @section('footer')
- {{-- <script src="{{asset('js/staff.js')}}"> --}}
  </script>
  @endsection
