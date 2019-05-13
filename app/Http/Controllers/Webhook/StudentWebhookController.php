47c6300071933730fe0a414a5b94b4f94e0de231
Nguyen Vu Dung <dungnv@nal.vn>
2019-05-13 17:48:22 +0700

    add webhook route

--- app/Http/Controllers/Webhook/StudentWebhookController.php
+++ app/Http/Controllers/Webhook/StudentWebhookController.php
@@ -16,4 +16,21 @@ class StudentWebhookController  extends WebhookController
 
         return response()->json(['REQUEST_ID' => $id,'RESULT' => 'OK']);    
     } 
+
+    public function edit_student(Request $request)
+    {
+        $inputs = $request->all();
+        $id = $inputs['id'];
+        
+        Log::debug('EDIT STUDENT HOOK:');
+        Log::debug(var_export($inputs, true));
+        return response()->json(['REQUEST_ID' => $id,'RESULT' => 'OK']);
+    }
+
+    public function delete_student($id)
+    {
+        Log::debug('DELETE STUDENT HOOK:');
+        Log::debug(var_export($id, true));
+        return response()->json(['REQUEST_ID' => $id,'RESULT' => 'OK']);
+    }
 }
