0fefbfbad4af94270a788d7e9e738961851215a4
Nguyen Vu Dung <dungnv@nal.vn>
2019-07-25 11:13:31 +0700

    add change password for logedin user

--- app/Staff.php
+++ app/Staff.php
@@ -1,70 +1,93 @@
 <?php
 namespace App;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
 class Staff extends Model
 {
     protected $table = 'staffs';
     /**
 
- * Create a function addStaff
- *
- * @return void
- */
-public static function addStaff($data){
-    $staff = DB::table('staffs')->insert([
-        ['name' => $data['name'],
-        'role' => $data['role'],
-        'email' => $data['email'],
-        'gender' => $data['gender'],
-        'address' => $data['address'],
-        'phone_number' => $data['phone_number'],
-        'birth_date' => $data['birth_date'],
-        'branch_id' => $data['branch_id'],
-        'created_at' => $data['created_at'],
-        'password' => bcrypt($data['password']),
-    ],
-]);
-    return $staff;
-}
-public static function search($keyword, $record_per_page,$page = 1){
-    $start = ($page - 1) * $record_per_page;
-    $search = DB::table('staffs')
-        ->leftJoin('branch', 'branch.id', '=', 'staffs.branch_id')
-        ->select('staffs.id','staffs.email','staffs.name','staffs.role','staffs.branch_id','staffs.birth_date','staffs.gender','staffs.address','staffs.phone_number','branch.branch_name')
-        ->orderBy('staffs.id','desc')
-        ->where('staffs.name','like','%'.$keyword.'%')
-        ->orwhere('staffs.email','like','%'.$keyword.'%')
-        ->orwhere('staffs.address','like','%'.$keyword.'%')
-        ->orwhere('staffs.phone_number','like','%'.$keyword.'%')
-        ->offset($start)->limit($record_per_page)->get();
-    return $search;
-}
+     * Create a function addStaff
+     *
+     * @return void
+     */
+    public static function addStaff($data)
+    {
+        $staff = DB::table('staffs')->insert([
+            ['name' => $data['name'],
+                'role' => $data['role'],
+                'email' => $data['email'],
+                'gender' => $data['gender'],
+                'address' => $data['address'],
+                'phone_number' => $data['phone_number'],
+                'birth_date' => $data['birth_date'],
+                'branch_id' => $data['branch_id'],
+                'created_at' => $data['created_at'],
+                'password' => bcrypt($data['password']),
+            ],
+        ]);
+        return $staff;
+    }
 
-public static function deleteStaff($staff_id){
-    return DB::table('staffs')->where('id',$staff_id)->delete();
-}
+    public static function search($keyword, $record_per_page, $page = 1)
+    {
+        $start = ($page - 1) * $record_per_page;
+        $search = DB::table('staffs')
+            ->leftJoin('branch', 'branch.id', '=', 'staffs.branch_id')
+            ->select('staffs.id', 'staffs.email', 'staffs.name', 'staffs.role', 'staffs.branch_id', 'staffs.birth_date', 'staffs.gender', 'staffs.address', 'staffs.phone_number', 'branch.branch_name')
+            ->orderBy('staffs.id', 'desc')
+            ->where('staffs.name', 'like', '%' . $keyword . '%')
+            ->orwhere('staffs.email', 'like', '%' . $keyword . '%')
+            ->orwhere('staffs.address', 'like', '%' . $keyword . '%')
+            ->orwhere('staffs.phone_number', 'like', '%' . $keyword . '%')
+            ->offset($start)->limit($record_per_page)->get();
+        return $search;
+    }
 
-public static function editPasswordStaff($id, $newPassword){
-    $Staff = DB::table('staffs')
-    ->where('id', $id)
-    ->update([
-        'password'=> bcrypt($newPassword),
-    ]);
-    return $Staff;
-}
-public static function editStaff( $data, $id){
-    $staff  = DB::table('staffs')
-    ->where('id', $id)
-    ->update([
-        'name' => $data['name'],
-        'email' => $data['email'],
-        'gender' => $data['gender'],
-        'birth_date' => $data['birth_date'],
-        'address' => $data['address'],
-        'phone_number' => $data['phone_number'],
-        'updated_at' => $data['updated_at'],
-    ]);
-    return $staff;
-}
+    public static function deleteStaff($staff_id)
+    {
+        return DB::table('staffs')->where('id', $staff_id)->delete();
+    }
+
+    public static function editPasswordStaff($id, $newPassword)
+    {
+        $staff = DB::table('staffs')
+            ->where('id', $id)
+            ->update([
+                'password' => bcrypt($newPassword),
+            ]);
+        return $staff;
+    }
+
+    public static function editStaff($data, $id)
+    {
+        $staff = DB::table('staffs')
+            ->where('id', $id)
+            ->update([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'gender' => $data['gender'],
+                'birth_date' => $data['birth_date'],
+                'address' => $data['address'],
+                'phone_number' => $data['phone_number'],
+                'updated_at' => $data['updated_at'],
+            ]);
+        return $staff;
+    }
+
+    public static function checkCurrentPassword ($password, $id) 
+    {
+        $staff = Staff::find($id);
+        
+        if (Hash::check($password, $staff->password)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,6 +81,9 @@
                 {{--  <div class="pull-left">
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>  --}}
+                <div class="pull-left">
+                  <a style="cursor:pointer" data-toggle="modal" data-target="#userchangepassword-modal">Đổi mật khẩu</a>
+                </div>
                 <div class="pull-right">
                   <a href="/logout" class="btn btn-default btn-flat">Thoát</a>
                 </div>
@@ -225,6 +228,47 @@
        immediately after the control sidebar -->
   <div class="control-sidebar-bg"></div>
 </div>
+
+<div class="modal fade" id="userchangepassword-modal" role="dialog" aria-hidden="true">
+  <div class="modal-dialog" role="document" style="width:400px">
+      <div class="modal-content">
+          <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
+              <h3 class="modal-title" id="userchangepassword-modal-title">Đổi mật khẩu</h3>
+          </div>
+          <div class="modal-body">
+              <div class="row"> 
+                <div class="col-sm-12">
+                  <form id="frmChangePasswd">
+                      <div class="form-group">
+                          <label for="currentPassword">Mật khẩu hiện tại</label>
+                          <input type="password" class="form-control" id="currentPassword" placeholder="Nhập mật khẩu đang sử dụng">
+                          <span class="help-block"></span>
+                      </div>
+                      <div class="form-group">
+                          <label for="newPassword">Mật khẩu mới</label>
+                          <input type="password" class="form-control" id="newPassword" placeholder="Nhập mật khẩu mới">
+                          <span class="help-block"></span>
+                      </div>
+                      <div class="form-group">
+                          <label for="newPasswordConfirm">Xác nhận mật khẩu mới</label>
+                          <input type="password" class="form-control" id="newPasswordConfirm" placeholder="Nhập lại mật khẩu mới">
+                          <span class="help-block"></span>
+                      </div>
+                  </form>
+                </div>
+              </div>
+          </div>
+          <div class="modal-footer">
+              <button type="button" class="btn btn-default" id="btnClose" data-dismiss="modal">Đóng</button>
+              <button type="button" class="btn btn-primary" id="btnChangePassword" data-loading-text="<i class='fa fa-spinner fa-spin'></i> Đang lưu...">Đổi mật khẩu</button>
+          </div>
+      </div>
+  </div>
+</div>
+
 <!-- ./wrapper -->
 <!-- jQuery 3 -->
 {{--  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>  --}}
