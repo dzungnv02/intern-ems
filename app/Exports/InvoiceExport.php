1c68c73c13964ae2ba73dd9334da3d11288c73d8
dzungnv02 <dungnv02@gmail.com>
2020-10-03 11:56:39 +0700

    add class InvoicesExport

---
+++ app/Exports/InvoiceExport.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+/**
+ * class InvoicesExport
+ * see https://docs.laravel-excel.com/3.1/exports/collection.html
+ */
+class InvoicesExport implements FromCollection, WithHeadings, WithEvents
+{
+
+    public function headings(): array
+    {
+        return [
+            'No.',
+            'Collecting Date',
+            'No of Recept',
+            'Code',
+            'Name',
+            'Parent Name',
+            'Payment Method',
+            'Course Name',
+            'Discount',
+            '',
+            '',
+            'Amount',
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [];
+    }
+
+    public function collection()
+    {
+
+    }
+
+    public function storeExcel()
+    {
+        //return Excel::store(new InvoicesExport, 'invoices.xlsx', 's3');
+    }
+
+}
